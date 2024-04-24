dcb7e653b7f4307af4f19ac7f96a8258bb6df0a7
luana.arruda <luanamarrudaa@gmail.com>
2024-04-24 19:28:03 -0300

    Add json-Server and Service

---
+++ src/app/product.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  url = "http://localhost:3000/products";
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);
+  }
+
+  save(product:Product): Observable<Product>{
+    return this.http.post<Product>(this.url, product);
+  }
+
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
 
   formGroupProduct: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(private formBuilder: FormBuilder, private service: ProductService){
     this.formGroupProduct = formBuilder.group({
       id : [''],
       name : [''],
@@ -23,8 +24,23 @@ export class ProductsComponent {
     });
   }
 
+    ngOnInit(): void {
+      this.loadProducts();
+    }
+
+      loadProducts(){
+        this.service.getProducts().subscribe({
+          next: data => this.products = data
+        });
+
+      }
+
   save(){
-    this.products.push(this.formGroupProduct.value);
+    this.service.save(this.formGroupProduct.value).subscribe({
+      next: data => this.products.push(data)
+    });
+
+
   }
 
 }
