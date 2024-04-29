0c63134b64fdbe3912436e2e9a13434c7f156885
luana.arruda <luanamarrudaa@gmail.com>
2024-04-29 20:03:41 -0300

    add validators

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,68 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
-
   products: Product[] = [];
   formGroupProduct: FormGroup;
   isEditing: boolean = false;
+  submited: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private service: ProductService){
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: ProductService
+  ) {
     this.formGroupProduct = formBuilder.group({
-      id : [''],
-      name : [''],
-      desc : [''],
-      price : [''],
-      qnt : ['']
+      id: [''],
+      name: ['', [Validators.minLength(3), Validators.required]],
+      desc: [''],
+      price: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      qnt: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
     });
   }
 
-    ngOnInit(): void {
-      this.loadProducts();
-    }
+  ngOnInit(): void {
+    this.loadProducts();
+  }
 
-      loadProducts(){
-        this.service.getProducts().subscribe({
-          next: data => this.products = data
+  loadProducts() {
+    this.service.getProducts().subscribe({
+      next: (data) => (this.products = data),
+    });
+  }
+
+  save() {
+    this.submited = true;
+    if (this.formGroupProduct.valid) {
+      if (this.isEditing) {
+        this.service.update(this.formGroupProduct.value).subscribe({
+          next: () => {
+            this.loadProducts();
+            this.isEditing = false;
+            this.submited = false;
+          },
         });
-
+      } else {
+        this.service.save(this.formGroupProduct.value).subscribe({
+          next: (data) => {
+            this.products.push(data);
+            this.submited = false;
+          }
+        });
       }
 
-  save(){
-    if(this.isEditing){
-      this.service.update(this.formGroupProduct.value).subscribe({
-        next: () => {
-          this.loadProducts();
-          this.isEditing = false;
-        }
-      })
+      this.formGroupProduct.reset();
     }
-    else{
-      this.service.save(this.formGroupProduct.value).subscribe({
-        next: data => this.products.push(data)
-      });
-    }
-
-    this.formGroupProduct.reset();
-
   }
-  delete(product:Product){
+  delete(product: Product) {
     this.service.delete(product).subscribe({
-      next: () => this.loadProducts()
+      next: () => this.loadProducts(),
     });
   }
 
-  edit(product:Product){
+  edit(product: Product) {
     this.formGroupProduct.setValue(product);
     this.isEditing = true;
   }
 
-}
+  get name(): any{
+    return this.formGroupProduct.get("name");
+  }
 
+  get price(): any{
+    return this.formGroupProduct.get("price")
+  }
+
+  get qnt(): any{
+    return this.formGroupProduct.get("qnt")
+  }
+
+
+
+}
